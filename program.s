4911506304d3d248548e47538adac73a0ca259da
Timothy Herchen <timothy.herchen@gmail.com>
2022-01-13 00:24:17 -0800

    OPTIMAL

--- program.s
+++ program.s
@@ -1,11 +1,6 @@
 
           global    start
 
-          section .data
-digit_table:                                ; table of powers of 2 in decimal, for base conversion, in packed word form
-          array dq `\00\00\00\00\00\00\00\00\00\01`, `\00\00\00\00\00\00\00\00\00\02`, `\00\00\00\00\00\00\00\00\00\04`, `\00\00\00\00\00\00\00\00\00\08`, `\00\00\00\00\00\00\00\00\00\10`, `\00\00\00\00\00\00\00\00\00\20`, `\00\00\00\00\00\00\00\00\00\40`, `\00\00\00\00\00\00\00\00\01\1c`, `\00\00\00\00\00\00\00\00\02\38`, `\00\00\00\00\00\00\00\00\05\0c`, `\00\00\00\00\00\00\00\00\0a\18`, `\00\00\00\00\00\00\00\00\14\30`, `\00\00\00\00\00\00\00\00\28\60`, `\00\00\00\00\00\00\00\00\51\5c`, `\00\00\00\00\00\00\00\01\3f\54`, `\00\00\00\00\00\00\00\03\1b\44`, `\00\00\00\00\00\00\00\06\37\24`, `\00\00\00\00\00\00\00\0d\0a\48`, `\00\00\00\00\00\00\00\1a\15\2c`, `\00\00\00\00\00\00\00\34\2a\58`, `\00\00\00\00\00\00\01\04\55\4c`, `\00\00\00\00\00\00\02\09\47\34`, `\00\00\00\00\00\00\04\13\2b\04`, `\00\00\00\00\00\00\08\26\56\08`, `\00\00\00\00\00\00\10\4d\48\10`, `\00\00\00\00\00\00\21\37\2c\20`, `\00\00\00\00\00\00\43\0a\58\40`, `\00\00\00\00\00\01\22\15\4d\1c`, `\00\00\00\00\00\02\44\2b\36\38`, `\00\00\00\00\00\05\24\57\09\0c`, `\00\00\00\00\00\0a\49\4a\12\18`, `\00\00\00\00\00\15\2f\30\24\30`, `\00\00\00\00\00\2a\5e\60\48\60`, `\00\00\00\00\00\55\59\5d\2d\5c`, `\00\00\00\00\01\47\4f\56\5b\54`, `\00\00\00\00\03\2b\3b\49\53\44`, `\00\00\00\00\06\57\13\2f\43\24`, `\00\00\00\00\0d\4a\26\5f\22\48`, `\00\00\00\00\1b\30\4d\5a\45\2c`, `\00\00\00\00\36\61\37\51\26\58`, `\00\00\00\01\09\5f\0b\3e\4d\4c`, `\00\00\00\02\13\5a\17\19\37\34`, `\00\00\00\04\27\50\2e\33\0b\04`, `\00\00\00\08\4f\3c\5d\02\16\08`, `\00\00\00\11\3b\15\56\04\2c\10`, `\00\00\00\23\12\2b\48\08\58\20`, `\00\00\00\46\24\57\2c\11\4c\40`, `\00\00\01\28\49\4a\58\23\35\1c`, `\00\00\02\51\2f\31\4c\47\06\38`, `\00\00\05\3e\5e\63\35\2a\0d\0c`, `\00\00\0b\19\59\63\06\54\1a\18`, `\00\00\16\33\4f\62\0d\44\34\30`, `\00\00\2d\03\3b\60\1b\25\04\60`, `\00\00\5a\07\13\5c\36\4a\09\5c`, `\00\01\50\0e\27\55\09\30\13\54`, `\00\03\3c\1c\4f\46\12\60\27\44`, `\00\07\14\39\3b\28\25\5c\4f\24`, `\00\0e\29\0f\12\50\4b\55\3a\48`, `\00\1c\52\1e\25\3d\33\47\11\2c`, `\00\39\40\3c\4b\17\03\2a\22\58`, `\01\0f\1d\15\32\2e\06\54\45\4c`, `\02\1e\3a\2b\00\5c\0d\45\27\34`, `\04\3d\10\56\01\54\1b\26\4f\04`, `\09\16\21\48\03\44\36\4d\3a\08`
-:num_to_digits:
-          db        "000123456789101112131415161718192021222324252627282930313233343536373839404142434445464748495051525354555657585960616263646566676869707172737475767778798081828384858687888990919293949596979899"
           section .text
 %define MAP_FAILED -1                       ; returned if mmap fails
 call_mmap:                                  ; takes in rax as the number of bytes to allocate
@@ -31,68 +26,137 @@ valid_size_request:
           syscall                           ; mmap (sets rax according to our conventions)
           ret
 u64_to_str:                                 ; convert rdi as a 64-bit integer, write decimal to [rsi] (no bounds checking...)
-                                            ; returns length in rax
-                                            ; clobbers rax, rbx, rdi, xmm0/1
-          push      rbp                     ; base pointer
-          mov       rbp, rsp
-                                            ; we repeatedly add decimal powers of two vertically
-                                            ; 0b1000101
-                                            ; ... 0 0 0 0 01
-                                            ; ... 0 0 0 0 04
-                                            ; ... 0 0 0 0 64
-                                            ;     0 0 0 0 69
-
-                                            ; then we perform the carry, and write the result into rsi
-          pxor      xmm0, xmm0              ; stores the decimal bytes (higher)
-          pxor      xmm1, xmm1              ; (lower)
-          mov       r8, digit_table         ; for some reason it complains about 32-bit addressing mode...
-          mov       rbx, 0
-vertical_add:
-          shr       rax, 1                  ; sets CF if 0 bit set
-          jnc       no_add_needed
-          phaddw    xmm0, oword [r8+rbx*8]
-          phaddw    xmm1, oword [r8+rbx*8+16]
-no_add_needed:
-          add       rbx, 4
-          cmp       rbx, 252                ; iterate 64 times
-          jl        vertical_add
-                                            ; CARRY TIME
-          mov       ebx, 0
-carry_gang1:
-          pextrb    ecx, xmm0, ebx 
-          add       ecx, '0'
-          mov       [rsi+rbx], cx
-          inc       bx
-          cmp       rbx, 15
-          jl        carry_gang1
-          mov       ebx, 0
-carry_gang2:
-          pextrb    ecx, xmm1, bx
-          add       ecx, '0'
-          mov       [rsi+rbx], cx
-          inc       bx
-          cmp       rbx, 15
-          jl        carry_gang2
-
-          mov       rax, 30
-
-          mov       rsp, rbp
-          pop       rbp                     ; preserve rbp
+          test      rdi, rdi
+          jnz       non_zero
+          mov       byte [rsi], '0'
+          mov       rax, 1
           ret
+non_zero:
+          xor       r8, r8                  ; length
+          inc       r8
+          cmp       rdi, 10
+          jle       length_calculated
+          inc       r8
+          cmp       rdi, 100
+          jle       length_calculated
+          inc       r8
+          cmp       rdi, 1000
+          jle       length_calculated
+          inc       r8
+          cmp       rdi, 10000
+          jle       length_calculated
+          inc       r8
+          cmp       rdi, 100000
+          jle       length_calculated
+          inc       r8
+          cmp       rdi, 1000000
+          jle       length_calculated
+          inc       r8
+          cmp       rdi, 10000000
+          jle       length_calculated
+          inc       r8
+          cmp       rdi, 100000000
+          jle       length_calculated
+          inc       r8
+          cmp       rdi, 1000000000
+          jle       length_calculated
+          inc       r8
+          mov       r9, 10000000000
+          cmp       r9, rdi
+          jle       length_calculated
+          inc       r8
+          mov       r9, 100000000000
+          cmp       r9, rdi
+          jle       length_calculated
+          inc       r8
+          mov       r9, 1000000000000
+          cmp       r9, rdi
+          jle       length_calculated
+          inc       r8
+          mov       r9, 10000000000000
+          cmp       r9, rdi
+          jle       length_calculated
+          inc       r8
+          mov       r9, 100000000000000
+          cmp       r9, rdi
+          jle       length_calculated
+          inc       r8
+          mov       r9, 1000000000000000
+          cmp       r9, rdi
+          jle       length_calculated
+          inc       r8
+          mov       r9, 10000000000000000
+          cmp       r9, rdi
+          jle       length_calculated
+          inc       r8
+          mov       r9, 100000000000000000
+          cmp       r9, rdi
+          jle       length_calculated
+          inc       r8
+          mov       r9, 1000000000000000000
+          cmp       r9, rdi
+          jle       length_calculated
+          inc       r8
+length_calculated:
+          mov       rax, rdi
+          mov       r9, rsi                 ; beginning of string
+          add       rsi, r8
+          mov       byte [rsi], r8b
+
+          mov       rbx, 10
+calc_digits:
+          xor       rdx, rdx
+          div       rbx                     ; rax is floored result, rdx is remainder
+          add       rdx, '0'
+          
+          mov       byte [rsi], dl
+          dec       rsi
+          cmp       r9, rsi
+          jl        calc_digits
+
+          mov       rax, r8
+          inc       rax
+          ret
+
+
+          ; returns length in rax
 start:
           
           mov       rax, 20000              ; 20 kilobytes
           call      call_mmap               ; get memory
           xchg      rax, rsi                ; rsi SHOULD be the beginning of the mmap-ed memory?
+          mov       r10, rsi
           mov       qword [rsi], "poop"     ; store poop
-          add       rsi, 4                  ; move forward
-          mov       rdi, 12345              ; store number
-          call      u64_to_str              ; convert rdi to decimal, storing in [rsi]
+          add       rsi, 4
 
+          mov       r11, 1 
+          mov       r12, 0 
+          mov       r13, 0
+fibonacci:
+          mov       rdi, r12
+          add       rdi, r11
+
+          call      u64_to_str
+
+
+          add       rsi, rax
+          mov       byte [rsi], `\n` 
+          inc       rsi
+
+          mov       r11, r12
+          mov       r12, rdi
+          inc       r13
+
+          cmp       r13, 20
+          jl        fibonacci
+
+          
           mov       rax, 0x02000004         ; system call for write
           mov       rdi, 1                  ; file handle 1 is stdout
-         ;mov       rsi, rsi                ; address of string to output?
-          mov       rdx, rax                  ; number of bytes
+          mov       rsi, r10                ; address of string to output?
+          mov       rdx, rsi
+          sub       rdx, r10
+          mov       rdx, 200
           syscall                           ; invoke operating system to do the write
           mov       rax, 0x02000001         ; system call for exit
           xor       rdi, rdi                ; exit code 0
