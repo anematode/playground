5f78db9c9a782c8c5359376307f82e6585a847a2
Timothy Herchen <timothy.herchen@gmail.com>
2022-05-14 18:38:05 -0700

    cont

--- mse2/mse.cc
+++ mse2/mse.cc
@@ -16,16 +16,43 @@ constexpr IType SEARCH_MAX = (_SEARCH_MAX / 1024 + 1) * 1024;
 alignas(32) std::bitset<SEARCH_MAX> reached;
 
 // x |-> 2x+1, 3x, 3x+2, 3x+7
-void init_1024() {
+void init_1536() {
     reached[1] = true;
 
-    for (IType i = 2; i < SEARCH_MAX; ++i) {
+    for (IType i = 2; i < 1536; ++i) { // 3 * 256 * 2
         if ((i % 2 == 1 && reached[((i - 1) / 2)]) ||
                 (i % 3 == 0 && reached[i / 3]) ||
                 (i > 7 && i % 3 == 1 && reached[(i - 7) / 3]) ||
                 (i % 3 == 2 && reached[(i - 2) / 3])) {
             reached[i] = true;
         }            
+    }
+}
+
+void init_rest() {
+    // We wish to implement a branchless and vectorized version of init_1024, processing things in 256-bit chunks. Alas,
+    // this is not trivial. We essentially generate four "streams" of 256-bit chunks (one for each of the mappings)
+    // constructed from memory, and unroll vehemently.
+    
+    char* _reached = reinterpret_cast<char*>(&reached);
+
+    for (IType i = 6; i < SEARCH_MAX / 256; i += 6) { // in 256-bit chunk units
+        void* write_addr = _reached + (i * 32);
+
+        char* flr2_addr = _reached + (i * 32) / 2;
+        char* flr3_addr = _reached + ((i / 3 - 1) * 32);
+
+        __m256 flr2_p1 = _mm256_load_si256((__m256i*) flr2_addr);
+        __m256 flr2_p2 = _mm256_load_si256((__m256i*) (flr2_addr + 32));
+
+        __m256 b = _mm256_setzero_si256();
+
+        uint64_t p = _pdep_u64(0, 0);
+
+        __m256 flr3_p1 = _mm256_load_si256((__m256i*) flr3_addr);
+
+
+
     }
 }
 
@@ -60,7 +87,10 @@ int main() {
         throw std::runtime_error("SEARCH_MAX must be a multiple of 1024");
     }
 
-    init_1024(); // init first 1024 results
+    init_1536(); // init first 1024 results
+
+    init_rest();
+
     for_each_entry([&] (IType i, bool r) {
             if (i < 10000 && !r && i % 4 == 3) {
             std::cout << i << '\n';
