363c4e7471ef36a050b782e91a0fb0753a047261
Timothy Herchen <timothy.herchen@gmail.com>
2022-05-16 13:35:21 -0700

    pclmulqdq enjoyer

--- mse2/mse.cc
+++ mse2/mse.cc
@@ -24,7 +24,13 @@ void init_1536() {
 	reached[1] = true;
 
 	for (IType i = 2; i < 1536; ++i) { // 3 * 256 * 2
-		if ((i % 2 == 1 && reached[((i - 1) / 2)]) ||
+
+		if (i % 5 == 0) {
+			reached[i] = true;
+		}
+			continue;
+
+		if (true || (i % 2 == 1 && reached[((i - 1) / 2)]) ||
 				(i % 3 == 0 && reached[i / 3]) ||
 				(i > 7 && i % 3 == 1 && reached[(i - 7) / 3]) ||
 				(i % 3 == 2 && reached[(i - 2) / 3])) {
@@ -35,43 +41,34 @@ void init_1536() {
 
 
 void init_rest() {
-	// We wish to implement a branchless and vectorized version of init_1024, processing things in 256-bit chunks. Alas,
-	// this is not trivial. We essentially generate four "streams" of 256-bit chunks (one for each of the mappings)
-	// constructed from memory, and unroll vehemently.
+	// We wish to implement a branchless and vectorized version of init_1024, processing things in 256-bit chunks.
 
-	char* _reached = reinterpret_cast<char*>(&reached);
+	uint64_t* _reached = reinterpret_cast<uint64_t*>(&reached);
 
-	for (IType i = 6; i < SEARCH_MAX / 256; i += 6) { // in 256-bit chunk units
-		void* write_addr = _reached + (i * 32);
+	for (IType _i = 1536; _i < SEARCH_MAX / 64; _i += 1536) { // i is index in bits
+		IType write_i = _i / 64;  // writing to this index in uint64_t
+		IType read2s = write_i / 2; // reading from here and spreading by 2 gives 2*x
+		IType read3s = write_i / 3 - 1; // reading from here will allow us to spread by 3 to get 3*x
 
-		char* flr2_addr = _reached + (i * 32) / 2;
-		char* flr3_addr = _reached + ((i / 3 - 1) * 32);
+		// 2x+1
+		__m128i read2 = _mm_load_si128(reinterpret_cast<const __m128i*>(_reached + read2s));
 
-		__m256 flr2_p1 = _mm256_load_si256((__m256i*) flr2_addr);
-		__m256 flr2_p2 = _mm256_load_si256((__m256i*) (flr2_addr + 32));
-		__m256 flr2_p3 = _mm256_load_si256((__m256i*) (flr2_addr + 64));
+		// Upper and lower halves of the result (2*x)
+		__m128i scale2p1 = _mm_clmulepi64_si128(read2, read2, 0x0);
+		__m128i scale2p2 = _mm_clmulepi64_si128(read2, read2, 0x11);
 
-#define _extract_fatten_part(mm, P) _pdep_u64(_mm256_extract_epi32(mm, 0), 0xaaaaaaaaaaaaaaaa)
-#define _fatten_low(mm) _mm256_set_epi64x( \
-			extract_fatten_part(mm, 0), \
-			extract_fatten_part(mm, 1), \
-			extract_fatten_part(mm, 2), \
-			extract_fatten_part(mm, 3) \
-		)
-#define _fatten_high(mm) _mm256_set_epi64x( \
-			extract_fatten_part(mm, 4), \
-			extract_fatten_part(mm, 5), \
-			extract_fatten_part(mm, 6), \
-			extract_fatten_part(mm, 7) \
-		)
-#define _extract_31_fatten_part(mm, P) _pdep_u64(_mm256_extract
+		// +1
+		uint64_t scale2p11 = _mm_extract_epi64(scale2p1, 0) << 1;
+		uint64_t scale2p12 = _mm_extract_epi64(scale2p1, 1) << 1;
+		uint64_t scale2p21 = _mm_extract_epi64(scale2p2, 0) << 1;
+		uint64_t scale2p22 = _mm_extract_epi64(scale2p2, 1) << 1;
 
-		__m256 k1 = fatten_low(flr2_p1);
+		std::cout << "hi" << scale2p11 << '\n';
 
-		__m256 flr3_p1 = _mm256_load_si256((__m256i*) flr3_addr);
-		__m256 flr3_p2 = _mm256_load_si256((__m256i*) flr3_addr);
-
-		
+		*(_reached + write_i) = scale2p11;
+		*(_reached + write_i + 1) = scale2p12;
+		*(_reached + write_i + 2) = scale2p21;
+		*(_reached + write_i + 3) = scale2p22;
 	}
 }
 
@@ -90,6 +87,8 @@ IType checksum(IType lim) {
 
 	return s;
 }
+
+// Checksum 100000: 15063046391347018756
 
 
 template <typename L>
@@ -113,8 +112,8 @@ int main() {
 	init_rest();
 
 	for_each_entry([&] (IType i, bool r) {
-			if (i < 10000 && !r && i % 4 == 3) {
-			//std::cout << i << '\n';
+			if (i > 1536 && r) {
+			  std::cout << i << '\n';
 			}
 			});
 
